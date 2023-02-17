93e559dbac2cb3f7ce7a357d47d95a521ce94868
Aadesh Kamble <aadesh10kamble@gmail.com>
2023-02-17 13:49:08 +0530

    Deploy

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ axios.defaults.contentType = 'application/json';
 // axios.defaults.baseURL = process.env.REACT_APP_ENV === 'production' ?
 //   process.env.REACT_APP_LIVE_API : process.env.REACT_APP_LOCAL_API;  
 axios.defaults.baseURL = process.env.REACT_APP_LIVE_API;
+console.log (axios.defaults.baseURL);
 axios.interceptors.request.use(function (config) {
   const token = localStorage.getItem('token');
   if (token) config.headers['x-access-token'] = token;
