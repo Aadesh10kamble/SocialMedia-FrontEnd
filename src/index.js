c426e5dbdead061e35959c04bd81e70797bcd8d3
Aadesh Kamble <aadesh10kamble@gmail.com>
2023-02-17 13:40:29 +0530

    Deploy

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import App from './App';
 import axios from 'axios';
 
 axios.defaults.contentType = 'application/json';
-axios.defaults.baseURL = process.env.NODE_ENV === 'production' ?
-  process.env.REACT_APP_LIVE_API : process.env.REACT_APP_LOCAL_API;
-
+// axios.defaults.baseURL = process.env.REACT_APP_ENV === 'production' ?
+//   process.env.REACT_APP_LIVE_API : process.env.REACT_APP_LOCAL_API;  
+axios.defaults.baseURL = process.env.REACT_APP_LIVE_API;
 axios.interceptors.request.use(function (config) {
   const token = localStorage.getItem('token');
   if (token) config.headers['x-access-token'] = token;
