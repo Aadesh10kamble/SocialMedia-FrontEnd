c9100bb52071a849c67b85ef87d4c513be228ca1
Aadesh Kamble <aadesh10kamble@gmail.com>
2023-02-17 11:48:48 +0530

    Deploy

--- src/helper.js
+++ src/helper.js
@@ -1,4 +1,4 @@
 export const splitWord = (word) => word.replace(/([a-z])([A-Z])/g, '$1 $2').replace(/^./, word[0].toUpperCase());
 export const descriptionLength = (description) => description.slice(0, 150);
-export const getProfilePicURL = (profilePic) => process.env.REACT_APP_PROFILE_URL + profilePic;
-export const getImageURL = (image) => process.env.REACT_APP_IMAGE_URL + image;
+export const getProfilePicURL = profilePic =>process.env.REACT_APP_LOCAL_API + 'profilePic/' + profilePic;
+export const getImageURL = image => process.env.REACT_APP_LOCAL_API + 'images/' + image;
